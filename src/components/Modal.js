6a5c8d3f4c40b0b47786070c181e911604c83d3a
Maria Soledad Lanfranconi <pennymsl@gmail.com>
2023-04-17 18:16:00 -0300

    cerrar el modal onClick

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { TodoContext } from "./TodoContext";
 import '../styles/Modal.css';
 
 function Modal({ children }) {
+  const { setOpenModal } = React.useContext(TodoContext)
+  const onCancel = (event) => {
+    if (event.target.classList.contains('ModalBackground')) {
+      setOpenModal(false)
+    }
+    event.nativeEvent.stopImmediatePropagation()
+  }
     return ReactDOM.createPortal(
-      <div className="ModalBackground">
+      <div className="ModalBackground"
+      onClick={onCancel}>
         {children}
       </div>,
       document.getElementById('modal')
